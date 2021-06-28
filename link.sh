b9b050bce291dffc062e0c72ef1cee0450ea9961
Jordan <zjzhang325@gmail.com>
2021-06-28 14:04:46 -0700

    Improve link script

--- link.sh
+++ link.sh
@@ -2,4 +2,6 @@ ln -sf ~/.dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+mkdir -p ~/.config/nvim
+ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 source ~/.bashrc
